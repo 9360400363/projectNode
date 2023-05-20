b088d186771741c1c14e75f1f7c436b54314b9ec
sanjai <124699183+9360400363@users.noreply.github.com>
2023-05-20 15:58:42 +0530

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -1,12 +1,1 @@
-const router = require("express").Router();
-const User = require("../models/User");
 
-router.post("/register", (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
-});
-
-module.exports = router;
